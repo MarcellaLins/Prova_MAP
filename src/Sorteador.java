437e8de790e0464d30e0023125f23baebf27ba14
laryssadrr <laryssadantas256@gmail.com>
2026-06-17 11:45:34 -0300

    update sorteador

--- src/Sorteador.java
+++ src/Sorteador.java
@@ -2,14 +2,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Sorteador {
-    public static Cartas sortear() {
-        ArrayList<Cartas> cartas = new ArrayList<>();
+    private static ArrayList<Cartas> cartas;
+
+    public Sorteador() {
+        cartas = new ArrayList<>();
 
         cartas.add(Cartas.MONSTRO);
         cartas.add(Cartas.MALDICAO);
         cartas.add(Cartas.RACA);
         cartas.add(Cartas.CLASSE);
+    }
 
+    public static Cartas sortear() {
         Collections.shuffle(cartas);
         return cartas.getFirst();
     }

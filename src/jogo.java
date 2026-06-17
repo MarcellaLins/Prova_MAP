1f1d70c0bcabb7d0d6cf4f7da07aedb805050b5f
laryssadrr <laryssadantas256@gmail.com>
2026-06-17 11:59:16 -0300

    jogo e main

--- src/jogo.java
+++ src/jogo.java
@@ -1,7 +1,34 @@
+import Strategy.*;
 
 public class jogo {
 
+        public static void chutarPorta() {
+            Sorteador sorteador = new Sorteador();
+            Cartas cartaSorteada = sorteador.sortear();
 
+            switch (cartaSorteada) {
+                case MONSTRO:
+                    System.out.println("Carta sorteada: MONSTRO");
+                    Monstro monstro = new Monstro();
+                    monstro.classificar();
+                    break;
+                case MALDICAO:
+                    System.out.println("Carta sorteada: MALDIÇÃO");
+                    Maldicao maldicao = new Maldicao();
+                    maldicao.classificar();
+                    break;
+                case RACA:
+                    System.out.println("Carta sorteada: RAÇA");
+                    Raca raca = new Raca();
+                    raca.classificar();
+                    break;
+                case CLASSE:
+                    System.out.println("Carta sorteada: CLASSE");
+                    Classe classe = new Classe();
+                    classe.classificar();
+                    break;
+            }
+        }
 
 
 }
